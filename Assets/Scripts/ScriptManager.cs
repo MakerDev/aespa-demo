f904872ea5d2e75b92c297a0dffdcbc07dfc6bb0
YujinShin <raki2001@naver.com>
2022-02-20 23:28:57 +0900

    assets updated

---
+++ Assets/Scripts/ScriptManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScriptManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro _emotionText;
+    [SerializeField]
+    private TextMeshPro _stateText;
+
+    public void SetEmotionText(string text)
+    {
+        _emotionText.SetText(text);
+    }
+
+    public void SetStateText(string text)
+    {
+        _stateText.SetText(text);
+    }
+}
---
+++ Assets/Scripts/StateTransitionSimulator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 실험자 상태들
+public enum SubjectStates
+{
+    TaskNotStartedYet,
+    Idle,
+    Calibrating,
+    WorkingOn,
+    TaskFinished
+}
+
+public class StateTransitionSimulator : MonoBehaviour
+{
+    public SubjectStates State { get; private set; } = SubjectStates.TaskNotStartedYet;
+    [SerializeField]
+    private ScriptManager _scriptManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
